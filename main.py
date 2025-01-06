cfc5747afeff09d9760e6e24cdd5b740f2fef2a3
alyssaolivia <jordan.alyss@gmail.com>
2025-01-05 22:59:43 -0400

    Updated with CRD operations

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-from fastapi import FastAPI
+from random import randrange
+from typing import Optional
+from fastapi import FastAPI, Response, status, HTTPException
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "Post 1", "content": "This is a post", "published": True, "rating": 5, "id" : 1}, {"title": "Post 2", "content": "This is another post", "published": False, "rating": 4, "id" : 2}]
+
+def fetch_posts(id: int):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+    return None
 
 @app.get("/")
 async def root():
@@ -8,5 +26,28 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is a post"}
-    
+    return {"data": my_posts}
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    print(post)
+    post_dict = post.dict()
+    post_dict["id"] = randrange(10000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}   
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    print(id)
+    post = fetch_posts(id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return {"post_detail": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    post = fetch_posts(id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    my_posts.remove(post)
+
