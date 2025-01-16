eeb89103bdc42fa57fa2b86caaf3d13ec3c75b0f
alyssaolivia <jordan.alyss@gmail.com>
2025-01-15 23:00:30 -0400

    Removed the environment variables

--- app/database.py
+++ app/database.py
@@ -2,8 +2,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
-DATABASE_URL = os.getenv("FastApi_DB_URL")
+DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine = create_engine(DATABASE_URL)
 
