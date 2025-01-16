2d92f97371daca0eb9fcc6718e348c26bd6350d5
alyssaolivia <jordan.alyss@gmail.com>
2025-01-15 23:00:47 -0400

    Added vote router

--- app/main.py
+++ app/main.py
@@ -3,14 +3,17 @@ from fastapi import FastAPI
 from passlib.context import CryptContext
 from . import models
 from .database import engine
-from .routers import user, post, auth
+from .routers import user, post, auth, vote
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 @app.get("/")
 async def root():
